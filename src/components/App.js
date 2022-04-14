4ef71de21285b7a505a3d55a60e1c54d3305f6d3
Axel <axel.caron@gmail.com>
2022-04-14 18:34:48 +0200

    Chapter2Part2

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "../App.css";
+import Cart from "./Cart";
+import Banner from "./Banner";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import logo from "../assets/logo.png";
+import "../styles/Banner.css";
+
+function Banner() {
+  const title = "La maison de la jungle";
+  return (
+    <div className="lmj-banner">
+      <img src={logo} alt="la maison jungle" className="lmj-logo"></img>
+      <h1 className="lmj-title">{title}</h1>
+    </div>
+  );
+}
+
+export default Banner;
