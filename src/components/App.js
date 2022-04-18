329478f10419ef8506e01c8d0a05b8dddfe85b04
Axel <axel.caron@gmail.com>
2022-04-18 12:00:17 +0200

    Chapter2Part3

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "../App.css";
 import Cart from "./Cart";
 import Banner from "./Banner";
+import ShoppingList from "./ShoppingList";
 
 function App() {
   return (
     <div>
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   );
 }
