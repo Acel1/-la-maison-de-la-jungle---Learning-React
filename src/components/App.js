41688e5cb06dd0b5a1f87fc5310100c34d9eb980
Axel <axel.caron@gmail.com>
2022-05-13 18:18:00 +0200

    add and subtract buttons in Cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,44 @@
-import { useState, useEffect } from 'react'
-import Banner from './Banner'
-import logo from '../assets/logo.png'
-import Cart from './Cart'
-import Footer from './Footer'
-import ShoppingList from './ShoppingList'
-import '../styles/Layout.css'
+import { useState } from "react"
+import Banner from "./Banner"
+import logo from "../assets/logo.png"
+import Cart from "./Cart"
+import Footer from "./Footer"
+import ShoppingList from "./ShoppingList"
+import "../styles/Layout.css"
 
 function App() {
-	const savedCart = localStorage.getItem('cart')
-	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
-	useEffect(() => {
-		localStorage.setItem('cart', JSON.stringify(cart))
-	}, [cart])
+  const [cart, updateCart] = useState([])
 
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart cart={cart} updateCart={updateCart} />
-				<ShoppingList cart={cart} updateCart={updateCart} />
-			</div>
-			<Footer />
-		</div>
-	)
+  function addItem(name, price) {
+    const currentPlantAdded = cart.find((plant) => plant.name === name)
+    if (currentPlantAdded) {
+      const cartFilteredCurrentPlant = cart.filter((plant) => plant.name !== name)
+      updateCart([...cartFilteredCurrentPlant, { name, price, amount: currentPlantAdded.amount + 1 }])
+    } else {
+      updateCart([...cart, { name, price, amount: 1 }])
+    }
+  }
+  function deleteItem(name, price) {
+    const currentPlantAdded = cart.find((plant) => plant.name === name)
+    const cartFilteredCurrentPlant = cart.filter((plant) => plant.name !== name)
+    updateCart([...cartFilteredCurrentPlant, { name, price, amount: currentPlantAdded.amount - 1 }])
+    if (currentPlantAdded.amount === 1) {
+      updateCart([...cartFilteredCurrentPlant])
+    }
+  }
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt='La maison jungle' className='lmj-logo' />
+        <h1 className='lmj-title'>La maison jungle</h1>
+      </Banner>
+      <div className='lmj-layout-inner'>
+        <Cart cart={cart} updateCart={updateCart} addItem={addItem} deleteItem={deleteItem} />
+        <ShoppingList addItem={addItem} />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
