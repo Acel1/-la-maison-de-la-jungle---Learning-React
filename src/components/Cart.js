c3a0ebbb98a4565ea61c86c66eab81b05b07f786
Axel <axel.caron@gmail.com>
2022-05-24 16:12:46 +0200

    s

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import { AiFillDelete } from "react-icons/ai"
 
 function Cart({ cart, updateCart, addItem, deleteItem, isOpen, setIsOpen }) {
